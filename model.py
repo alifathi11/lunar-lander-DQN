d63db66c4c86de287b0f06eab7324c6b4a5d2166
alifathi11 <dot.alifathi@gmail.com>
2026-07-08 17:58:00 +0330

    Implement model.py

---
+++ model.py
@@ -0,0 +1,24 @@
+from torch import nn
+
+class QNetwork(nn.Module):
+    def __init__(self, state_dim, action_dim, hidden_dim=128): 
+        super.__init__()
+
+        self.net = nn.Sequential(
+            nn.Linear(state_dim, hidden_dim),
+            nn.ReLU(),
+
+            nn.Linear(hidden_dim, hidden_dim),
+            nn.ReLU(),
+
+            nn.Linear(hidden_dim, action_dim)
+        )
+        
+    def forward(self, x):
+        return self.net(x)
+    
+def create_policy_net(state_dim, action_dim, hidden_dim=128):
+    return QNetwork(state_dim, action_dim, hidden_dim)
+
+def create_target_net(state_dim, action_dim, hidden_dim=128):
+    return QNetwork(state_dim, action_dim, hidden_dim)
